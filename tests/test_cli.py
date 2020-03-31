975df056fda351d0f76b5aac24c2ccf3defb83df
Bryce Weiner <bryce.weiner@alt.market>
2020-03-31 04:55:24 -0700

    Initial commit

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,7 +2,7 @@ import pytest
 
 import docker
 
-import tmn as package
+import taomn as package
 
 client = docker.from_env()
 
@@ -15,12 +15,12 @@ def runner():
 
 
 @pytest.fixture
-def tmn():
-    from tmn import tmn
-    from tmn.environments import environments
-    from tmn import configuration
+def taomn():
+    from taomn import taomn
+    from taomn.environments import environments
+    from taomn import configuration
     environments['devnet'] = {
-        'tomochain': {
+        'tao': {
             'BOOTNODES': (
                 'test'
             ),
@@ -36,14 +36,14 @@ def tmn():
         }
     }
     environments['testnet'] = environments['devnet']
-    configuration.resources.init('tomochain', 'tomo_tests')
-    return tmn
+    configuration.resources.init('tao', 'tao_tests')
+    return taomn
 
 
-def _clean(tmn):
-    from tmn import configuration
+def _clean(taomn):
+    from taomn import configuration
     try:
-        client.containers.get('test1_tomochain').remove(force=True)
+        client.containers.get('test1_tao').remove(force=True)
     except Exception:
         pass
     try:
@@ -55,43 +55,43 @@ def _clean(tmn):
     except Exception:
         pass
     try:
-        client.networks.get('test1_tmn').remove()
+        client.networks.get('test1_taomn').remove()
     except Exception:
         pass
-    configuration.resources.init('tomochain', 'tomo_tests')
+    configuration.resources.init('tao', 'tao_tests')
     configuration.resources.user.delete('id')
     configuration.resources.user.delete('name')
     configuration.resources.user.delete('net')
 
 
-def test_version(runner, tmn):
+def test_version(runner, taomn):
     version = '0.5.1'
-    result = runner.invoke(tmn.main, ['--version'])
+    result = runner.invoke(taomn.main, ['--version'])
     assert result.output[-6:-1] == version
     assert package.__version__ == version
 
 
-def test_error_docker(runner, tmn):
-    result = runner.invoke(tmn.main, ['--docker', 'unix://test', 'docs'])
+def test_error_docker(runner, taomn):
+    result = runner.invoke(taomn.main, ['--docker', 'unix://test', 'docs'])
     assert '! error: could not access the docker daemon\nNone\n'
     assert result.exit_code == 0
 
 
-def test_command(runner, tmn):
-    result = runner.invoke(tmn.main)
+def test_command(runner, taomn):
+    result = runner.invoke(taomn.main)
     assert result.exit_code == 0
 
 
-def test_command_docs(runner, tmn):
-    result = runner.invoke(tmn.main, ['docs'])
+def test_command_docs(runner, taomn):
+    result = runner.invoke(taomn.main, ['docs'])
     msg = 'Documentation on running a masternode:'
-    link = 'https://docs.tomochain.com/masternode/tmn/\n'
+    link = 'https://docs.tao.network/masternode/taomn/\n'
     assert result.output == "{} {}".format(msg, link)
     assert result.exit_code == 0
 
 
-def test_command_start_init_devnet(runner, tmn):
-    result = runner.invoke(tmn.main, [
+def test_command_start_init_devnet(runner, taomn):
+    result = runner.invoke(taomn.main, [
         'start', '--name', 'test1', '--net',
         'devnet', '--pkey',
         '0123456789012345678901234567890123456789012345678901234567890123'
@@ -100,11 +100,11 @@ def test_command_start_init_devnet(runner, tmn):
     assert 'Starting masternode test1:' in lines
     for line in lines:
         assert '✗' not in line
-    _clean(tmn)
+    _clean(taomn)
 
 
-def test_command_start_init_testnet(runner, tmn):
-    result = runner.invoke(tmn.main, [
+def test_command_start_init_testnet(runner, taomn):
+    result = runner.invoke(taomn.main, [
         'start', '--name', 'test1', '--net',
         'testnet', '--pkey',
         '0123456789012345678901234567890123456789012345678901234567890123'
@@ -113,11 +113,11 @@ def test_command_start_init_testnet(runner, tmn):
     assert 'Starting masternode test1:' in lines
     for line in lines:
         assert '✗' not in line
-    _clean(tmn)
+    _clean(taomn)
 
 
-def test_command_start_init_mainnet(runner, tmn):
-    result = runner.invoke(tmn.main, [
+def test_command_start_init_mainnet(runner, taomn):
+    result = runner.invoke(taomn.main, [
         'start', '--name', 'test1', '--net',
         'mainnet', '--pkey',
         '0123456789012345678901234567890123456789012345678901234567890123'
@@ -126,142 +126,142 @@ def test_command_start_init_mainnet(runner, tmn):
     assert 'Starting masternode test1:' in lines
     for line in lines:
         assert '✗' not in line
-    _clean(tmn)
+    _clean(taomn)
 
 
-def test_command_start_init_invalid_name(runner, tmn):
-    result = runner.invoke(tmn.main, [
+def test_command_start_init_invalid_name(runner, taomn):
+    result = runner.invoke(taomn.main, [
         'start', '--name', 'tes', '--net', 'devnet', '--pkey', '1234'])
     lines = result.output.splitlines()
     assert 'error' in lines[1]
     assert '! error: --name is not valid' in lines
-    _clean(tmn)
+    _clean(taomn)
 
 
-def test_command_start_init_no_pkey(runner, tmn):
-    result = runner.invoke(tmn.main, [
+def test_command_start_init_no_pkey(runner, taomn):
+    result = runner.invoke(taomn.main, [
         'start', '--name', 'test1', '--net', 'devnet'])
     lines = result.output.splitlines()
     assert ('! error: --pkey is required when starting a new '
             'masternode') in lines
-    _clean(tmn)
+    _clean(taomn)
 
 
-def test_command_start_init_invalid_pkey_len(runner, tmn):
-    result = runner.invoke(tmn.main, [
+def test_command_start_init_invalid_pkey_len(runner, taomn):
+    result = runner.invoke(taomn.main, [
         'start', '--name', 'test1', '--net', 'devnet', '--pkey',
         '0123456789012345678901234567890123456789012345678901234567890'])
     lines = result.output.splitlines()
     assert '! error: --pkey is not valid' in lines
-    _clean(tmn)
+    _clean(taomn)
 
 
-def test_command_start_init_no_net(runner, tmn):
-    result = runner.invoke(tmn.main, [
+def test_command_start_init_no_net(runner, taomn):
+    result = runner.invoke(taomn.main, [
         'start', '--name', 'test1', '--pkey',
         '0123456789012345678901234567890123456789012345678901234567890123'])
     lines = result.output.splitlines()
     assert '! error: --net is required when starting a new masternode' in lines
-    _clean(tmn)
+    _clean(taomn)
 
 
-def test_command_start_init_no_name(runner, tmn):
-    result = runner.invoke(tmn.main, [
+def test_command_start_init_no_name(runner, taomn):
+    result = runner.invoke(taomn.main, [
         'start', '--pkey',
         '0123456789012345678901234567890123456789012345678901234567890123'])
     lines = result.output.splitlines()
     assert ('! error: --name is required when starting a new '
             'masternode') in lines
-    _clean(tmn)
+    _clean(taomn)
 
 
-def test_command_start(runner, tmn):
-    runner.invoke(tmn.main, [
+def test_command_start(runner, taomn):
+    runner.invoke(taomn.main, [
         'start', '--name', 'test1', '--net',
         'devnet', '--pkey',
         '0123456789012345678901234567890123456789012345678901234567890123'
     ])
-    result = runner.invoke(tmn.main, ['start'])
+    result = runner.invoke(taomn.main, ['start'])
     lines = result.output.splitlines()
     assert 'Starting masternode test1:' in lines
     for line in lines:
         assert '✗' not in line
-    _clean(tmn)
+    _clean(taomn)
 
 
-def test_command_start_ignore(runner, tmn):
-    result = runner.invoke(tmn.main, [
+def test_command_start_ignore(runner, taomn):
+    result = runner.invoke(taomn.main, [
         'start', '--name', 'test1', '--net',
         'devnet', '--pkey',
         '0123456789012345678901234567890123456789012345678901234567890123'
     ])
-    result = runner.invoke(tmn.main, ['start', '--name', 'test'])
+    result = runner.invoke(taomn.main, ['start', '--name', 'test'])
     lines = result.output.splitlines()
     assert '! warning: masternode test1 is already configured' in lines
-    _clean(tmn)
+    _clean(taomn)
 
 
-def test_command_stop(runner, tmn):
-    runner.invoke(tmn.main, [
+def test_command_stop(runner, taomn):
+    runner.invoke(taomn.main, [
         'start', '--name', 'test1', '--net',
         'devnet', '--pkey',
         '0123456789012345678901234567890123456789012345678901234567890123'
     ])
-    result = runner.invoke(tmn.main, ['stop'])
+    result = runner.invoke(taomn.main, ['stop'])
     lines = result.output.splitlines()
     assert 'Stopping masternode test1:' in lines
     for line in lines:
         assert '✗' not in line
-    _clean(tmn)
+    _clean(taomn)
 
 
-def test_command_status(runner, tmn):
-    runner.invoke(tmn.main, [
+def test_command_status(runner, taomn):
+    runner.invoke(taomn.main, [
         'start', '--name', 'test1', '--net',
         'devnet', '--pkey',
         '0123456789012345678901234567890123456789012345678901234567890123'
     ])
-    result = runner.invoke(tmn.main, ['status'])
+    result = runner.invoke(taomn.main, ['status'])
     lines = result.output.splitlines()
     assert 'Masternode test1 status:' in lines
-    _clean(tmn)
+    _clean(taomn)
 
 
-def test_command_inspect(runner, tmn):
-    runner.invoke(tmn.main, [
+def test_command_inspect(runner, taomn):
+    runner.invoke(taomn.main, [
         'start', '--name', 'test1', '--net',
         'devnet', '--pkey',
         '0123456789012345678901234567890123456789012345678901234567890123'
     ])
-    result = runner.invoke(tmn.main, ['inspect'])
+    result = runner.invoke(taomn.main, ['inspect'])
     lines = result.output.splitlines()
     assert 'Masternode test1 details:' in lines
-    _clean(tmn)
+    _clean(taomn)
 
 
-def test_command_update(runner, tmn):
-    runner.invoke(tmn.main, [
+def test_command_update(runner, taomn):
+    runner.invoke(taomn.main, [
         'start', '--name', 'test1', '--net',
         'devnet', '--pkey',
         '0123456789012345678901234567890123456789012345678901234567890123'
     ])
-    result = runner.invoke(tmn.main, ['update'])
+    result = runner.invoke(taomn.main, ['update'])
     lines = result.output.splitlines()
     assert 'Updating masternode test1:' in lines
     for line in lines:
         assert '✗' not in line
-    _clean(tmn)
+    _clean(taomn)
 
 
-def test_command_remove(runner, tmn):
-    runner.invoke(tmn.main, [
+def test_command_remove(runner, taomn):
+    runner.invoke(taomn.main, [
         'start', '--name', 'test1', '--net',
         'devnet', '--pkey',
         '0123456789012345678901234567890123456789012345678901234567890123'
     ])
-    result = runner.invoke(tmn.main, ['remove', '--confirm'])
+    result = runner.invoke(taomn.main, ['remove', '--confirm'])
     lines = result.output.splitlines()
     assert 'Removing masternode test1:' in lines
     for line in lines:
         assert '✗' not in line
-    _clean(tmn)
+    _clean(taomn)
--- tmn/__init__.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-__version__ = '0.5.1'
-
-handler = logging.StreamHandler()
-handler.setFormatter(logging.Formatter('[%(levelname)s] %(message)s'))
-logger = logging.getLogger('tmn')
-logger.addHandler(handler)
-logger.setLevel('CRITICAL')
