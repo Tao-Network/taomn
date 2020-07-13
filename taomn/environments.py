a4f22cc310c1cd84211f722b11b5784fbf90a5d6
Bryce Weiner <bryce.weiner@alt.market>
2020-07-12 17:09:54 -0700

    Taoification

--- taomn/environments.py
+++ taomn/environments.py
@@ -2,14 +2,9 @@ environments = {
     'mainnet': {
         'tao': {
             'BOOTNODES': (
-                'enode://97f0ca95a653e3c44d5df2674e19e9324ea4bf4d47a46b1d8560f'
-                '3ed4ea328f725acec3fcfcb37eb11706cf07da669e9688b091f1543f89b24'
-                '25700a68bc8876@3.212.20.0:30301,enode://b72927f349f3a27b78'
-                '9d0ca615ffe3526f361665b496c80e7cc19dace78bd94785fdadc270054ab'
-                '727dbb172d9e3113694600dd31b2558dd77ad85a869032dea@188.166.207'
-                '.189:30301,enode://c8f2f0643527d4efffb8cb10ef9b6da4310c5ac9f2'
-                'e988a7f85363e81d42f1793f64a9aa127dbaff56b1e8011f90fe9ff57fa02'
-                'a36f73220da5ff81d8b8df351@104.248.98.60:30301'
+                'enode://7b3717f1318eb10e0e04a2590741fd9521a80d8bf5bb715d6507618d'
+                '930f9d94a2a8edeff812e8d0e4406fa573d176c004e71e7f142aeeb0887568758'
+                '52c68c8@149.28.69.174:20202'
             ),
             'NETSTATS_HOST': 'wss://stats.tao.network',
             'NETSTATS_PORT': '443',
@@ -22,20 +17,15 @@ environments = {
     'testnet': {
         'tao': {
             'BOOTNODES': (
-                'enode://4d3c2cc0ce7135c1778c6f1cfda623ab44b4b6db55289543d48ec'
-                'fde7d7111fd420c42174a9f2fea511a04cf6eac4ec69b4456bfaaae0e5bd2'
-                '36107d3172b013@52.221.28.223:30301,enode://ce1191bf9a634e7939'
-                '676d136816ad84941b079c03d6a96e64cca35852363012169055c6879c644'
-                'e821dc236a01d0499a1b7ff39e9518dbc00da87c7f1898604@13.251.101.'
-                '216:30301,enode://46dba3a8721c589bede3c134d755eb1a38ae7c5a4c6'
-                '9249b8317c55adc8d46a369f98b06514ecec4b4ff150712085176818d18f5'
-                '9a9e6311a52dbe68cff5b2ae@13.250.94.232:30301'
+                'enode://7b3717f1318eb10e0e04a2590741fd9521a80d8bf5bb715d6507618d'
+                '930f9d94a2a8edeff812e8d0e4406fa573d176c004e71e7f142aeeb0887568758'
+                '52c68c8@149.28.69.174:20202'
             ),
             'NETSTATS_HOST': 'stats.testnet.tao.network',
             'NETSTATS_PORT': '443',
-            'NETWORK_ID': '89',
+            'NETWORK_ID': '559',
             'WS_SECRET': (
-                'anna-coal-flee-carrie-zip-hhhh-tarry-laue-felon-rhine'
+                'anna-coal-flee-carrie-zip-hhhh-tarry'
             )
         },
     },
